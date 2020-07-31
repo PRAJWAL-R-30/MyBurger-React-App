65afe9c58b5269b3e5ad89682133bfc1e54a6637
PRAJWAL-R-30 <56631039+PRAJWAL-R-30@users.noreply.github.com>
2020-07-31 10:03:00 +0530

    Routing-Forms

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,6 +27,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount () {
+        console.log(this.props);
         axios.get('https://react-my-burger-dcfbc.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data})
@@ -88,30 +89,21 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        //alert('You continue!');
-        this.setState({ loading: true})
-        const data = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Prajwal R',
-                address: {
-                    city: 'Udupi',
-                    street: 'Thallur',
-                    pincode: '576230',
-                },
-                contact_no: 8861066802,
-                email: 'prajwalr3005@gmail.com',
-            },
-            deliveryMethod: 'fastest',
+        
+        
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', data)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false })
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false })
-            });
+
+        queryParams.push("price=" + this.state.totalPrice);
+
+        const queryString = queryParams.join('&');
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString,
+        })
     }
 
     render () {
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,21 +9,21 @@ const withErrorHandler = (WrappedComponent, axios ) => {
             error: null,
         }
 
-        componentWillMount () {
-            this.reqInterceptor = axios.interceptors.request.use(req => {
-                this.setState({ error: null });
-                return req;
-            })
-            this.resInterceptor = axios.interceptors.response.use( res => res, error => {
-                this.setState({ error: error });
-                console.log(error.message);
-            })
-        }
-        componentWillUnmount () {
-            axios.interceptors.request.eject(this.reqInterceptor);
-            axios.interceptors.response.eject(this.resInterceptor);
+        // UNSAFE_componentWillMount () {
+        //     this.reqInterceptor = axios.interceptors.request.use(req => {
+        //         this.setState({ error: null });
+        //         return req;
+        //     })
+        //     this.resInterceptor = axios.interceptors.response.use( res => res, error => {
+        //         this.setState({ error: error });
+        //         console.log(error.message);
+        //     })
+        // }
+        // UNSAFE_componentWillUnmount () {
+        //     axios.interceptors.request.eject(this.reqInterceptor);
+        //     axios.interceptors.response.eject(this.resInterceptor);
 
-        }
+        // }
 
         errorConfirmedHandler = () => {
             this.setState({ error: null });
